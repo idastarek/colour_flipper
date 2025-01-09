9acdcf91a71d7e23b2561f23ef92d0184bfa2861
Ida <ida.starek@gmail.com>
2025-01-09 19:45:38 +0000

    changing colour after clicking  button

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-// JS
+const colors = ["green", "blue", "pink", "rgba(123, 122, 200)", "#15025"];
+const btn = document.getElementById("click-me-btn");
+
+// listening for button clicks to change the background color and name
+btn.addEventListener("click", ()=> {
+    const randomColor = colors[Math.floor(Math.random()*colors.length)];  
+    document.querySelector(".current-color").innerHTML = randomColor;
+    document.querySelector("main").style.background = randomColor;
+})
