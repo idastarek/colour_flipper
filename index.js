eee2f3790829fa5b79f742dda8436a67894ebaba
Ida <ida.starek@gmail.com>
2025-01-19 13:15:48 +0000

    added comments to index js

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 // index.html logic
 
+// array of color values for index.html
 const colors = ["green", "blue", "pink", "rgba(123, 122, 200)", "#15025"];
 const btn = document.getElementById("click-me-btn");
 
 // listening for button clicks to change the background color and name
 btn.addEventListener("click", ()=> {
+    // generate a random color from the array
     const randomColor = colors[Math.floor(Math.random()*colors.length)];  
     document.querySelector(".current-color").innerHTML = randomColor;
     document.querySelector("main").style.backgroundColor = randomColor;
